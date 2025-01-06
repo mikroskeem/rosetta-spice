63d574e851f3047160d5c0db2f3b48441d33a208
usertam <code@usertam.dev>
2025-01-07 01:59:15 +0800

    rosetta: 24C5079e -> 24C101

--- rosetta.nix
+++ rosetta.nix
@@ -2,11 +2,11 @@
 let
   # https://swscan.apple.com/content/catalogs/others/index-rosettaupdateauto-1.sucatalog.gz
 
-  # Sequoia 15.2 Beta 3
-  version = "24C5079e";
+  # Sequoia 15.2
+  version = "24C101";
   src = fetchurl {
-    url = "https://swcdn.apple.com/content/downloads/38/30/072-18030-A_FANJVJ91EE/eeicx7eyga9aqrh2b3uif0yxqw7rn8dfuw/RosettaUpdateAuto.pkg";
-    hash = "sha256-rmShRTbhbMf+mKsQiKMss8uspnudnPeuxukcqCDY2wc=";
+    url = "https://swcdn.apple.com/content/downloads/56/11/072-44352-A_TZG2YTBMAZ/hnqfqmuuulu2yf3n226fqu7phar2jpago2/RosettaUpdateAuto.pkg";
+    hash = "sha256-fP/4HbPkMUz1LQrK1bJ0aEp+U1q7GYiCzY6/b5OptoY=";
   };
 
   drv = stdenv.mkDerivation {
